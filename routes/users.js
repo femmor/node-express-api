2e88d330ebcfd7f766a14f1e866ce363cc942f18
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-20 15:55:30 -0500

    added user to the users array

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,32 @@ import express from "express"
 // use router from express
 const router = express.Router()
 
+// Users
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 32
+    }
+]
+
 // Users routes
 // all routes in here will start with "/users"
 router.get("/", (req, res) => {
-    res.send("Hello from users")
+    res.send(users)
 })
+
+// add users in db
+router.post("/", (req, res) => {
+    const user = req.body
+    users.push(user)
+    res.send("user "+ user.firstName +" has been added to the database")
+})
+
 
 export default router
